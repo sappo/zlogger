da2757d3efedb8d09ec63117a926a85d31e3f42d
Kevin Sapper <kevin.b.sapper@student.hs-rm.de>
2016-06-08 14:48:24 +0200

    Problem: It is difficult to build locally
    Solution: Create script that sets the correct parameters

---
+++ build_prepare.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+BUILD_PREFIX=/users/k_sapper/.install
+
+CONFIG_OPTS=()
+CONFIG_OPTS+=("CFLAGS=-I${BUILD_PREFIX}/include")
+CONFIG_OPTS+=("CPPFLAGS=-I${BUILD_PREFIX}/include")
+CONFIG_OPTS+=("CXXFLAGS=-I${BUILD_PREFIX}/include")
+CONFIG_OPTS+=("LDFLAGS=-L${BUILD_PREFIX}/lib")
+CONFIG_OPTS+=("PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig")
+CONFIG_OPTS+=("--prefix=${BUILD_PREFIX}")
+CONFIG_OPTS+=("--with-docs=no")
+CONFIG_OPTS+=("--quiet")
+
+./autogen.sh 2> /dev/null
+./configure --enable-drafts=yes "${CONFIG_OPTS[@]}"
